7abb2a02f8b0c816bb498964a82ba598a8b5c1ce
Shujun-He <38227229+Shujun-He@users.noreply.github.com>
2020-12-07 16:56:34 -0600

    Add files via upload

---
+++ src/openvaccine/run.sh
@@ -0,0 +1,6 @@
+for i in {0..4};do
+python train.py --gpu_id 0 --kmer_aggregation --nmute 0 --epochs 75 --nlayers 5 \
+--batch_size 16 --kmers 5 --lr_scale 0.1 --path ../../../data --workers 2 \
+--dropout 0.1 --nclass 5 --ntoken 15 --nhead 32 --ninp 256 --nhid 1024 --warmup_steps 600 \
+--fold $i --weight_decay 0.1 --nfolds 10 --error_alpha 0.5 --noise_filter 0.25
+done
