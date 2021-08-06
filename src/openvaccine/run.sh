1052b05bbbf53c8d8f434115e794a270f19c5256
Shujun-He <38227229+Shujun-He@users.noreply.github.com>
2021-08-05 21:48:09 -0500

    Update run.sh

--- src/openvaccine/run.sh
+++ src/openvaccine/run.sh
@@ -1,4 +1,4 @@
-for i in {0..4};do
+for i in {0..9};do
 python train.py --gpu_id 0 --kmer_aggregation --nmute 0 --epochs 75 --nlayers 5 \
 --batch_size 16 --kmers 5 --lr_scale 0.1 --path ../../../data --workers 2 \
 --dropout 0.1 --nclass 5 --ntoken 15 --nhead 32 --ninp 256 --nhid 1024 --warmup_steps 600 \
