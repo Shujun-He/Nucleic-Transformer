12b57ca1c309f8ea8eded2f9d76d02eb98e24ec2
Shujun He <ukrakim@gmail.com>
2022-12-14 13:11:54 -0800

    removed code that is not needed

--- src/OpenVaccine/predict.sh
+++
@@ -1,4 +0,0 @@
-python predict.py --gpu_id 0,1 --kmer_aggregation --nmute 0 --epochs 100 --nlayers 5 \
---batch_size 64 --kmers 5 --lr_scale 0.1 --path ../../../data \
---dropout 0.1 --nclass 5 --ntoken 15 --nhead 32 --ninp 256 --nhid 1024 --warmup_steps 600 \
---fold 0 --weight_decay 1e-4 --nfolds 10
--- src/OpenVaccine/pretrain.sh
+++
@@ -1,6 +0,0 @@
-for i in {0..0};do
-python pretrain.py --gpu_id 0,1 --kmer_aggregation --nmute 0 --epochs 200 --nlayers 5 \
---batch_size 96 --kmers 5 --lr_scale 0.1 --path .. --workers 2 \
---dropout 0.1 --nclass 5 --ntoken 15 --nhead 32 --ninp 256 --nhid 1024 --warmup_steps 600 \
---fold $i --weight_decay 0.1
-done
